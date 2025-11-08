33e3dcc933295690b19e7c54f110af6a8ec1e2bf
NGUYEN-MINH-TRUC <nmtruc.study@gmail.com>
2025-11-09 00:47:58 +0700

    Update frontend

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PronaFlow_MVC.Models;
 
 namespace PronaFlow_MVC.Controllers
 {
     public class DashboardController : Controller
     {
-        // GET: Dashboard
+        private readonly PronaFlow_DBContext _db = new PronaFlow_DBContext();
+        
+        
+        /// <summary>
+        /// Displayed Dashboard main page
+        /// </summary>
+        /// <returns>View Index.cshtml with statitics data</returns>
         [HttpGet]
         public ActionResult Index()
         {
+            int totalProjects = _db.projects.Count();
+            int totalTasks = _db.tasks.Count();
+
+            ViewBag.TotalProjects = totalProjects;
+            ViewBag.TotalTasks = totalTasks;
+
+            ViewBag.Titel = "Dashboard | PronaFlow";
+
             return View();
         }
     }
